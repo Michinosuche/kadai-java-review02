513b110cc4df9f1f5eeed4e4228d4a982371c4fb
Michinosuche <takemonami@yahoo.com>
2023-01-29 17:14:49 +0900

    Javaプログラミング入門学習終了

---
+++ src/UseVariable.java
@@ -0,0 +1,13 @@
+
+public class UseVariable {
+
+    public static void main(String[] args) {
+            int number2;
+            String str1;
+            number2 = 1;
+            str1 = "abc";
+            System.out.println(number2);
+            System.out.println(str1);
+    }
+
+}
